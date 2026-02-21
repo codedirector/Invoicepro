efa1a19de19ddfe892e82facbc2f1aeed1898848
codedirector <shobhitsharma102003@gmail.com>
2026-02-21 14:50:37 +0530

    Fix error

--- app/api/invoices/[id]/status/route.ts
+++ app/api/invoices/[id]/status/route.ts
@@ -1,21 +1,23 @@
 import { auth } from "@/lib/auth"
 import { prisma } from "@/lib/prisma"
-import { NextResponse } from "next/server"
-import { headers } from "next/headers"
+import { NextRequest, NextResponse } from "next/server"
+
 export async function PATCH(
-  _: Request,
-  { params }: { params: { id: string } }
-) {
-   const session = await auth.api.getSession({
-     headers: await headers() // you need to pass the headers object.
- })
+  request: NextRequest,
+  context: { params: Promise<{ id: string }> } ) {
+  const { id } = await context.params 
+
+  const session = await auth.api.getSession({
+    headers: request.headers, 
+  })
+
   if (!session) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 })
   }
 
   const invoice = await prisma.invoice.findFirst({
     where: {
-      id: params.id,
+      id,
       userId: session.user.id,
     },
   })
